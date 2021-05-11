51ad1c62c525d31b4dd3599452ed80dd073cae4f
deepthi <deepthibhogaraju.3@gmail.com>
2021-05-11 19:16:34 +0530

    this assignment is in class given by mentor

---
+++ src/AssignmentString.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class AssignmentString {
+    public static void main(String []args){
+        String [][] students = {{"Bob","100"},
+                                {"Mike", "35"},
+                                {"Bob", "100"},
+                                {"Jason","35"},
+                                {"Mike", "55"},
+                                {"Jessica", "99"} };
+       HashMap<String,String> hashmap = new HashMap<String,String>();
+        Map.Entry<String,Integer> maxPrice = null;
+
+        for( int i =0; i< students.length;i++) {
+           String key = students[i][0];
+           int total =0, count=0;
+            if(hashmap.containsKey(key)){
+                count++;
+                total = ( Integer.parseInt(students[i][1]) + Integer.parseInt(hashmap.get(key)) ) /2;
+                hashmap.put(students[i][0], String.valueOf( total ));
+            }
+            else{
+                hashmap.put(students[i][0], students[i][1]);
+            }
+       }
+        int temp=0; String result="";
+        for (Map.Entry<String, String> entry : hashmap.entrySet()) {  // Itrate through hashmap
+            int value = Integer.parseInt( entry.getValue() );
+
+            if( temp < value){
+                result = entry.getKey();
+                temp = value;
+            }
+        }
+        System.out.print(result);
+    }
+}
