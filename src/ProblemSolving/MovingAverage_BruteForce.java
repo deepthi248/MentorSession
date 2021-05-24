595eb8afac2471ffc91194db086e7e2f5c57affd
deepthi <deepthibhogaraju.3@gmail.com>
2021-05-24 18:59:08 +0530

    Added mentor approach -- optimised my code

--- src/ProblemSolving/MovingAverage_BruteForce.java
+++ src/ProblemSolving/MovingAverage_BruteForce.java
@@ -7,23 +7,22 @@ public class MovingAverage_BruteForce {
         // the resultant array should always be of length ---- " n-k+1 "
         double result [] = new double[arr.length-k+1];
 
-        /*
-        My approach -- using for and while by iterating over the given array
+
+      //  My approach -- using for and while by iterating over the given array
         for( int i =0;i<= arr.length-k ;i++){
-            int temp = 0,count=i;
-               double     average= 0;
+            int temp=0; double average= 0;
             while(temp < k){
-              //  System.out.println(  arr[count]);
-                average += arr[count++];
+              //  System.out.println(  arr[i+temp]);
+                average += arr[i+temp];
                 temp++;
             }
           //  System.out.println("average"+ average);
             result[index] = average/k;
             index++;
 
-        }*/
+        }
 
-        for ( int i =0; i< result.length;i++){
+        /*for ( int i =0; i< result.length;i++){
             double sum=0;
 
             for( int j=i; j < i+k;j++){
@@ -31,7 +30,7 @@ public class MovingAverage_BruteForce {
             }
 
             result[i]=sum/k;
-        }
+        }*/
         for( double value:result)
                 System.out.print(value+"  ");
     }
