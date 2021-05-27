5753a3fffb82cca04f9242e04c7882d806d90769
deepthi <deepthibhogaraju.3@gmail.com>
2021-05-27 18:57:23 +0530

    This program returns the largest subarray with unique elements

--- src/ProblemSolving/LongestSubstringWithoutRepitition_slidingWindow.java
+++ src/ProblemSolving/LongestSubstringWithoutRepitition_slidingWindow.java
@@ -5,24 +5,29 @@ import java.util.Map;
 
 public class LongestSubstringWithoutRepitition_slidingWindow {
     public static void main(String[] args) {
-    String str = "poiuytttr";
+    String str = "puikjlwwwwwkew";
     System.out.print(longestSubstring(str));
     }
     public static int longestSubstring(String str){
-        int startIndex=0, length =Integer.MIN_VALUE;
-        Map<Character,Integer> myMap = new HashMap<>();
-        for( int endIndex=0; endIndex < str.length(); endIndex++){
-            Character left = str.charAt(endIndex);
+        int startIndex = 0, length = Integer.MIN_VALUE;
+        Map<Character, Integer> myMap = new HashMap<>();
+        if( str.length() == 0 || str.equals("") || str.equals(" ")){
+            length=0;
+        }
+        else {
 
-            if(myMap.containsKey(left)){
-                startIndex++;
-                for( int i =0; i<=endIndex;i++)
-                    myMap.remove(str.charAt(i));
+            for (int endIndex = 0; endIndex < str.length(); endIndex++) {
+                Character left = str.charAt(endIndex);
+
+                if (myMap.containsKey(left)) {
+                    startIndex++;
+                    for (int i = 0; i <= endIndex; i++)
+                        myMap.remove(str.charAt(i));
+                } else {
+                    myMap.put(left, 1);
+                }
+                length = Math.max(myMap.size(), length);
             }
-            else{
-                myMap.put(left,1);
-            }
-           length = Math.max(myMap.size(), length);
         }
         return length;
     }
